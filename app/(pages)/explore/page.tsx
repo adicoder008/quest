233f4077160ee1cff436ab0ca5555b79c02c7564
Swati-V-Bhat <swativinayakbhat.221ds031@nitk.edu.in>
2025-08-18 19:20:00 +0530

    just for testing

---
+++ app/(pages)/explore/page.tsx
@@ -0,0 +1,14 @@
+//testing
+import Index from '@/components/Feed/Index'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <Index/>
+      
+    </>
+  )
+}
+
+export default page
