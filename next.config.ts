8a5ba5e7b71809a923032ddd9b6d83b9d928c758
Swati Vinayak Bhat <swativinayakbhat.221ds031@nitk.edu.in>
2025-11-04 16:44:43 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+const { IgnorePlugin } = require('webpack');
+
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    // This solves the "Module not found: Can't resolve '@remotion/compositor-..." errors
+    config.plugins.push(
+      new IgnorePlugin({
+        resourceRegExp: /^@remotion\/compositor-/,
+      })
+    );
+    return config;
+  }
 };
 
 export default nextConfig;
